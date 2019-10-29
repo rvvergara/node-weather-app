a5792a8371c79dbe974e58843c59840645c00c66
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-29 10:41:39 +0800

    Use destructuring in utils files

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,12 +3,14 @@ const request = require('request');
 const foreCast = (lat, long, callback) => {
   const url = `https://api.darksky.net/forecast/11ccf9b69a2309d3b0c033170e50db89/${lat},${long}?units=si`;
 
-  request({ url, json: true }, (err, res) => {
+  request({ url, json: true }, (err, { body }) => {
     if (err) {
       callback(err);
     } else {
-      const { temperature, precipProbability } = res.body.currently;
-      callback(err, { temperature, precipProbability, summary: res.body.daily.summary });
+      const { currently, daily } = body;
+      const { summary } = daily;
+      const { temperature, precipProbability } = currently;
+      callback(err, { temperature, precipProbability, summary });
     }
   });
 };
